d224165ba367dcd9f49a4ea891d28df15e175347
davinwong <davinwong@outlook.com>
2013-09-30 23:34:15 -0400

    listen to input file and print top bids

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-import sys, heapq
+import sys, time, os, heapq
 
 # command line arguments
 history = sys.argv[1]
@@ -6,12 +6,8 @@ control_input = sys.argv[2]
 control_output = sys.argv[3]
 bid_stream_1 = sys.argv[4]
 
-# print history
-# print control_input
-# print control_output
-# print bid_stream_1
-
 # min heap with negative numbers
+# heapq.heapify(history_data)
 bid_heap = []
 heapq.heapify(bid_heap)
 
@@ -22,10 +18,55 @@ for line in history_file:
 history_file.close()
 
 
-# heapq.heapify(history_data)
+""" taken from stack overflow"""
+
+#Set the filename and open the file
+file = open(control_input,'r')
+
+#Find the size of the file and move to the end
+st_results = os.stat(control_input)
+st_size = st_results[6]
+file.seek(st_size)
+
+while 1:
+    where = file.tell()
+    line = file.readline()
+    if not line:
+        time.sleep(1)
+        file.seek(where)
+    else:
+        line.strip()
+        # print line, # already has newline
+        input_command = line.split()
+        print input_command
+        try:
+            # print input_command[0]
+            total_bids = int(input_command[1])
+            bids_requested = int(input_command[2])
+            if input_command[0] == 'top':
+                print "len(bid_heap)"
+                print len(bid_heap)
+                if len(bid_heap) == total_bids:
+                    print heapq.nsmallest(bids_requested, bid_heap)
+                else:
+                    print "old data"
+        except:
+            pass
+        
+            # if input_command[0] == 'top':
+            #   print bid_heap.size()
 
 
-while bid_heap:
-    print -1*heapq.heappop(bid_heap)
+# remove from heap, add them back
+# print heapq.nsmallest(3, bid_heap)
 
-print bid_heap
+
+
+
+
+
+
+# while bid_heap:
+#     print -1*heapq.heappop(bid_heap)
+
+# print bid_heap
