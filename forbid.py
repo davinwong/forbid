5b1dfcffeeeb6c99aca390ef727a14efed73537e
davinwong <davinwong@outlook.com>
2013-10-01 17:47:22 -0400

    link interface and bid_market, fixed interface input from .txt file

--- forbid.py
+++ forbid.py
@@ -10,17 +10,18 @@ def main():
     control_output = sys.argv[3]
     bid_stream_1 = sys.argv[4]
 
-    interface = Interface(control_input, control_output, bid_stream_1)
-
     bid_market = BidMarket()
     bid_market.import_history(history)
 
-    print 'go'
-    print bid_market.top_bids(5, 3)
-    print 'no'
-    print bid_market.top_bids(4, 3)
+    interface = Interface(bid_market, control_input, control_output, bid_stream_1)
+    interface.open_control_input()
 
-    print bid_market.bid_heap
+    # print 'go'
+    # print bid_market.top_bids(5, 3)
+    # print 'no'
+    # print bid_market.top_bids(4, 3)
+
+    # print bid_market.bid_heap
 
 if __name__ == "__main__":
     main()
--- interface.py
+++ interface.py
@@ -1,41 +1,37 @@
 import sys, time, os, heapq, threading, Queue
 
 class Interface:
-    def __init__(self, control_input, control_output, bid_stream_1):
+    def __init__(self, bid_market, control_input, control_output, bid_stream_1):
+        self.bid_market = bid_market
         self.control_input = control_input
         self.control_output = control_output
         self.bid_stream_1 = bid_stream_1
 
-    # def open_control_input():
-    #     """ some stack overflow code"""
+    def open_control_input(self):
+        """ some stack overflow code"""
 
-    #     #Set the filename and open the file
-    #     file = open(control_input,'r')
+        #Set the filename and open the file
+        file = open(self.control_input,'r')
 
-    #     #Find the size of the file and move to the end
-    #     st_results = os.stat(control_input)
-    #     st_size = st_results[6]
-    #     file.seek(st_size)
+        #Find the size of the file and move to the end
+        st_results = os.stat(self.control_input)
+        st_size = st_results[6]
+        file.seek(st_size)
 
-    #     while 1:
-    #         where = file.tell()
-    #         line = file.readline()
-    #         if not line:
-    #             time.sleep(1)
-    #             file.seek(where)
-    #         else:
-    #             line.strip()
-    #             # print line, # already has newline
-    #             input_command = line.split()
-    #             print input_command
-    #             try:
-    #                 # print input_command[0]
-    #                 total_bids = int(input_command[1])
-    #                 requested_bids = int(input_command[2])
-    #                 if input_command[0] == 'top':
-    #                     if len(bid_heap) == total_bids:
-    #                         print heapq.nsmallest(requested_bids, bid_heap)
-    #                     else:
-    #                         print "old data"
-    #             except:
-    #                 pass
+        while 1:
+            where = file.tell()
+            line = file.readline()
+            if not line:
+                time.sleep(1)
+                file.seek(where)
+            else:
+                line.strip()
+                input_command = line.split()
+
+                try:
+                    total_bids = int(input_command[1])
+                    requested_bids = int(input_command[2])
+                    if input_command[0] == 'top':
+                        print self.bid_market.top_bids(total_bids, requested_bids)
+                except:
+                    pass
